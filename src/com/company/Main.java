7f7ae4811abf2d0abe39bf21088d9124a40c0eef
OndrejFixmer <73097549+OndrejFixmer@users.noreply.github.com>
2021-09-09 13:52:19 +0200

    Add files via upload

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,13 +7,13 @@ public class Main {
     public static void main(String[] args) {
 	Scanner sc = new Scanner(System.in);
 
-	int cislo = sc.nextInt();
+	double cislo = sc.nextDouble();
 	float vysledekfrajer = 0;
-        for (int i = 0; i < cislo+1; i++) {
-            float bum = (float) 1 / i;
+        for (int i = 1; i < cislo+1; i++) {
+            double bum = (double) 1 / i;
             vysledekfrajer += bum;
             System.out.println(1 + "/" + i + "=" + bum);
         }
-        System.out.println("cisl: " + vysledekfrajer);
+        System.out.println("cislo: " + vysledekfrajer);
     }
 }
